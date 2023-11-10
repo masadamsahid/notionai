2f04c0fb9bc7aef5d736bd4bad34a1b4bd0e76de
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-11-10 14:59:28 +0700

    dashboard notes list

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,22 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
-import { UserButton } from "@clerk/nextjs";
+import { auth, UserButton } from "@clerk/nextjs";
 import { Separator } from "@/components/ui/separator";
 import CreateNoteDialog from "@/components/CreateNoteDialog";
+import { db } from "@/lib/db";
+import { $notes } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
 
 type DashboardPageProps = {}
 
-const DashboardPage = (props: DashboardPageProps) => {
+const DashboardPage = async (props: DashboardPageProps) => {
+  
+  const { userId } = auth();
+  const notes = await db.select().from($notes).where(
+    eq($notes.userId, userId!)
+  );
+  
   return (
     <>
       <div className="grainy min-h-screen">
@@ -23,7 +32,7 @@ const DashboardPage = (props: DashboardPageProps) => {
               <div className="w-4"></div>
               <h1 className="text-3xl font-bold text-gray-900">My Notes</h1>
               <div className="w-4"></div>
-              <UserButton />
+              <UserButton/>
             </div>
           </div>
           
@@ -32,14 +41,40 @@ const DashboardPage = (props: DashboardPageProps) => {
           <div className="h-8"></div>
           
           {/*  List of all the notes */}
-          <div className="text-center">
-            <h2 className="text-xl text-gray-500">You have no notes yet.</h2>
-          </div>
+          {notes.length <= 0 && (
+            <div className="text-center">
+              <h2 className="text-xl text-gray-500">You have no notes yet.</h2>
+            </div>
+          )}
           
           <div className="grid sm:grid-cols-3 md:grid-cols-5 grid-cols-1 gap-3">
             <CreateNoteDialog/>
+            {notes.map((note) => {
+              return (
+                <a href={`/notes/${note.id}`} key={note.id}>
+                  <div
+                    className="overflow-hidden flex flex-col hover:shadow-xl transition hover:-translate-y-1 rounded-lg border border-stone-200">
+                    <img
+                      width={400}
+                      height={200}
+                      alt={note.name}
+                      src={note.imageUrl || ""}
+                    />
+                    <div className="p-4">
+                      <h3 className="text-xl font-semibold text-gray-900">
+                        {note.name}
+                      </h3>
+                      <div className="h-1"></div>
+                      <p className="text-sm text-gray-500">
+                        {new Date(note.createdAt).toLocaleDateString()}
+                      </p>
+                    </div>
+                  </div>
+                </a>
+              );
+            })}
           </div>
-          
+        
         </div>
       </div>
     </>
